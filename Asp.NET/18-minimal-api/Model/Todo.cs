358f469f94c986e1ae2802e8d3c784e7f1b1d52c
mukund58 <cursedscripting@gmail.com>
2026-02-25 09:46:04 +0530

    add Model

---
+++ Asp.NET/18-minimal-api/Model/Todo.cs
@@ -0,0 +1,7 @@
+namespace _18_minimal_api.Model;
+
+class Todo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }=string.Empty;
+}
--- Asp.NET/18-minimal-api/Program.cs
+++ Asp.NET/18-minimal-api/Program.cs
@@ -1,3 +1,5 @@
+using _18_minimal_api.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,10 +19,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-var items = new List<string> {
-    "apple",
-    "banana"
-};
+var items = new List<Todo>();
+
 app.MapGet("/todo", () => {
     return Results.Ok(items);
 });
@@ -35,39 +35,47 @@ app.MapGet("/todo/{id:int}", (int id) => {
 });
 
 app.MapPut("/todo/{id:int}",(int id,string data) => {
-    if (data == "" || data is null || data == " " || data.Contains("\t"))
+    if (data is null )
     {
         return Results.BadRequest();
     }
+    data = data.ToLower().Trim();
+
+    if (data.Length == 0) return Results.BadRequest("Invalid item");
+    
     if (id < 0) {
         return Results.BadRequest("Invalid ID");
     } else if (items.Count <= id) {
         return Results.NotFound("Item Not Found");
     }
     items[id] = data;
-    return Results.Created($"/todo/{items[id]}",data);
+    return Results.NoContent();
 
 });
 app.MapPost("/todo", (string data) => {
-    if (data == "" || data is null || data == " " || data.Contains("\t"))
+    if (data is null )
     {
         return Results.BadRequest();
     }
     data = data.ToLower().Trim();
+    if(data.Length == 0)  return Results.BadRequest("Invalid item");
+
     bool found = items.Contains(data);
     if(found){
         return Results.BadRequest();
     }
     items.Add(data);
-    return Results.Created($"/todo/{items.IndexOf(data+1)}",data);
+    return Results.Created($"/todo/{items.Count -1}",data);
 });
 
 app.MapDelete("/todo", (string data) => {
-    if (data == "" || data is null || data == " " || data.Contains("\t"))
+    if (data is null )
     {
         return Results.BadRequest();
     }
     data = data.ToLower().Trim();
+    if(data.Length == 0)  return Results.BadRequest("Invalid item");
+
     bool found = items.Contains(data);
     if(!found){
         return Results.NotFound("Item Not Found");

aec13848ba49dc7e46dda7871534bbf3b32f6803
mukund58 <cursedscripting@gmail.com>
2026-03-07 17:00:22 +0530

    add jwt with role

--- Asp.NET/19-jwt-Auth/Program.cs
+++ Asp.NET/19-jwt-Auth/Program.cs
@@ -48,7 +48,7 @@ namespace AuthenticationDemo
 
             app.UseAuthentication();
             app.UseAuthorization();
-''
+
             // ---------------------
             // SIGNUP / LOGIN (demo)
             // ---------------------.
